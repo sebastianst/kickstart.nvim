f6b56a127a2fcd5dfd6916e637b1bba2f0b54905
Sebastian Stammler <seb@oplabs.co>
2026-05-28 09:37:33 +0000

    add formatting toggle

--- init.lua
+++ init.lua
@@ -549,9 +549,22 @@ require('lazy').setup({
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>tf',
+        function()
+          vim.g.disable_autoformat = not vim.g.disable_autoformat
+          vim.notify('Format on save ' .. (vim.g.disable_autoformat and 'disabled' or 'enabled'))
+        end,
+        desc = '[T]oggle [F]ormat on save',
+      },
+    },
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return nil
+        end
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
